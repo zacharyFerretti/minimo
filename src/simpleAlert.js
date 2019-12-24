8641d276757b2c93e387ca8028b0942c9a24de98
Zachary Ferretti <zf66728@gmail.com>
2019-12-24 15:17:31 -0500

    Beginning to work on adding custom Javascript.

---
+++ src/simpleAlert.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById("MYBUTTON");
+  // onClick's logic below:
+  button.addEventListener("click", function() {
+    alert("HELLO");
+  });
+});
